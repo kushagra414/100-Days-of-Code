fedfd2651c817c64cf9ffb06a00f26d47f4f23f6
Kushagra <kushagrashekhawat414@gmail.com>
2020-01-15 22:56:39 +0530

    Completed Day2 Tasks. DSA and GSOC

---
+++ KushagraShekhawat/Programs/6. Finding middle element in a linked list.cpp
@@ -0,0 +1,92 @@
+// { Driver Code Starts
+// C program to find n'th Node in linked list
+#include <stdio.h>
+#include <stdlib.h>
+#include<iostream>
+using namespace std;
+
+/* Link list Node */
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+void append(struct Node** head_ref, struct Node **tail_ref, int new_data)
+{
+    struct Node* new_node = new Node(new_data);
+    
+    if (*head_ref == NULL)
+       *head_ref = new_node;
+    else
+       (*tail_ref)->next = new_node;
+    *tail_ref = new_node;
+}
+
+
+/* Function to get the middle of the linked list*/
+int getMiddle(struct Node *head);
+
+/* Driver program to test above function*/
+int main()
+{
+  int T,i,n,l;
+
+    cin>>T;
+
+    while(T--){
+    struct Node *head = NULL,  *tail = NULL;
+
+        cin>>n;
+        for(i=1;i<=n;i++)
+        {
+            cin>>l;
+            append(&head, &tail, l);
+        }
+
+    printf("%d\n", getMiddle(head));
+    }
+    return 0;
+}
+// } Driver Code Ends
+/* Link list Node 
+struct Node {
+    int data;
+    Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+}; */
+
+/* Should return data of middle node. If linked list is empty, then  -1*/
+int getMiddle(Node *head)
+{
+   struct Node *next_node_pointer = head;
+   int size_of_linked_list = 0,mid = 0,mid_element;
+   // Finding size of Linked List
+   while(next_node_pointer != NULL){
+       next_node_pointer = next_node_pointer->next;
+       size_of_linked_list++;
+   }
+   
+   // Calculate mid of Linked List
+   mid = (size_of_linked_list)/2;
+   
+   //Again traverse Linked List to find the mid element
+   while(mid!=0){
+       head = head->next;
+       mid--;
+       if(mid == 0)
+           mid_element = head->data;
+   }
+   return mid_element;
+   
+}
--- KushagraShekhawat/README.md
+++ KushagraShekhawat/README.md
@@ -8,6 +8,8 @@ I will be participating in following Activities in 100 Days of Code.
 - [x] DSA
 - [x] GSOC
 
+---
+
 > Day 1:
 >> Data Structure(Compulsory)
 - [x] [Subarray with given sum](https://practice.geeksforgeeks.org/problems/subarray-with-given-sum/0)
@@ -18,3 +20,16 @@ I will be participating in following Activities in 100 Days of Code.
 - [x] [Majority Elementy](https://practice.geeksforgeeks.org/problems/majority-element/0)
 >> GSOC
 - [x] Give time to GSOC.
+
+---
+
+> Day 2:
+>> Data Structure(Compulsory)
+- [x] [Finding middle element in a linked list](https://practice.geeksforgeeks.org/problems/finding-middle-element-in-a-linked-list/1)
+- [x] [Reverse a linked list](https://practice.geeksforgeeks.org/problems/reverse-a-linked-list/1)
+- [x] [Detect Loop in linked list](https://practice.geeksforgeeks.org/problems/detect-loop-in-linked-list/1)
+>> Algorithms(Optional)
+- [x] [Sort an array of 0s, 1s and 2s](https://practice.geeksforgeeks.org/problems/sort-an-array-of-0s-1s-and-2s/0)
+- [x] [Chocolate Distribution Problem](https://practice.geeksforgeeks.org/problems/chocolate-distribution-problem/0)
+>> GSOC
+- [x] Work on some issues and try to fix them.
