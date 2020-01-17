692b7fcc8614a7463e4e629e414bda6bba46cee1
janmaijai rana <56648968+Maverick-99@users.noreply.github.com>
2020-01-17 22:51:33 +0530

    Create queue using array.cpp

---
+++ Janmaijai rana/Day 4/queue using array.cpp
@@ -0,0 +1,87 @@
+// { Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+struct QueueNode
+{
+    int data;
+    QueueNode *next;
+};
+
+class MyQueue {
+private:
+    int arr[10001];
+    int front;
+    int rear;
+
+public :
+    MyQueue(){front=0;rear=0;}
+    void push(int);
+    int pop();
+};
+
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        MyQueue *sq = new MyQueue();
+
+        int Q;
+        cin>>Q;
+        while(Q--){
+        int QueryType=0;
+        cin>>QueryType;
+        if(QueryType==1)
+        {
+            int a;
+            cin>>a;
+            sq->push(a);
+        }else if(QueryType==2){
+            cout<<sq->pop()<<" ";
+
+        }
+        }
+        cout<<endl;
+    }
+    }
+// } Driver Code Ends
+/* 
+
+The structure of the class is
+class MyQueue {
+private:
+    int arr[10001];
+    int front;
+    int rear;
+
+public :
+    MyQueue(){front=0;rear=0;}
+    void push(int);
+    int pop();
+};
+ */
+
+/* The method push to push element into the queue */
+void MyQueue :: push(int x)
+{
+    arr[rear]=x;
+     rear++;
+     
+}
+
+/*The method pop which return the element 
+  poped out of the queue*/
+int MyQueue :: pop()
+{
+    int p=0;
+    
+    if(front==rear)
+    return -1;
+    
+    p=arr[front];
+    front++;
+    return p;
+    
+}
