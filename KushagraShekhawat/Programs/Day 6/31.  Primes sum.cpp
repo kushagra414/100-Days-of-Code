a5b8db30ecd17b18b36e81f139dfaa5942c0dada
Kushagra <kushagrashekhawat414@gmail.com>
2020-01-20 00:15:39 +0530

    Completed 3 Questions Dsa, Day 6

---
+++ KushagraShekhawat/Programs/Day 6/31.  Primes sum.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <stdio.h>
+#include <bits/stdc++.h>
+#include <math.h>
+using namespace std;
+
+int main() {
+	int T;
+	scanf("%d",&T);
+	while(T--){
+	    int N;
+	    scanf("%d",&N);
+	    if(N%2==0)
+	        printf("Yes\n");
+	    else{
+	        N=N-2;
+	        bool prime[N+1];
+	        memset(prime,true,sizeof(prime));
+	        for(int p=2;p*p<=N;p++){
+	            if(prime[p]==true){
+	                for(int i=p*p;i<=N;i=i+p)
+	                    prime[i] = false;
+	            }
+	        }
+	    if(prime[N])
+	        printf("Yes\n");
+	    else
+	        printf("No\n");
+	    }
+	}
+	return 0;
+}
