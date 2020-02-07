c12cda2195830ebb6e18549f0a0a948be050c37c
Deepak Kumar <deepakku28@gmail.com>
2020-02-07 22:22:48 +0530

    Deepak (#86)
    * completed day2 task , DSA
    
    * completed 2 program of day3
    
    * completed 2 program of day3
    
    * completed 2 program of day4
    
    * completed 2 program of day 6
    
    * completed 1 program of dsc of day 7
    
    * completed 1 program of DSA
    
    * completed two task of DSA
    
    * completed 1 program of DSA
    
    * completed 1 task of DSA
    
    * completed few task of day 4 and day 5

---
+++ Deepak Kumar/Programs/Day 5/5.1.Height of Binary Tree.cpp
@@ -0,0 +1,130 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+    
+    return temp;
+}
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+        
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+                
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+int height(struct Node* node);
+
+int main()
+{
+    int t;
+	scanf("%d ",&t);
+    while(t--)
+    {
+        string s;
+		getline(cin,s);
+		Node* root = buildTree(s);
+		cout<<height(root)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
+/* Tree node structure  used in the program
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};*/
+
+/* Computes the height of binary tree with given root.  */
+int height(Node* node)
+{
+    if(node==NULL){
+        return 0;
+    }
+    else{
+        int left_height=height(node->left);
+        int right_height=height(node->right);
+        if(left_height>right_height){
+            return(left_height+1);
+        }
+        else{
+            return(right_height+1);
+        }
+    }
+}
+
+
