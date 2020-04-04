1bbda9f96bde75c14f4020074b67702a87a04ad0
Kushagra <kushagrashekhawat414@gmail.com>
2020-04-04 21:13:41 +0530

    Completed Day 82. #163

---
+++ KushagraShekhawat/Programs/Day 82/181. Extreme nodes in alternate order.cpp
@@ -0,0 +1,141 @@
+// { Driver Code Starts
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+Node* newNode(int val)
+{
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+    
+    return temp;
+}
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+        
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+                
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+void printExtremeNodes(Node* root);
+
+int main()
+{
+    int t;
+	scanf("%d ",&t);
+    while(t--)
+    {
+        string s;
+		getline(cin,s);
+		Node* root = buildTree(s);
+		
+        printExtremeNodes(root);
+   		cout<<endl;
+  }
+  return 0;
+}
+// } Driver Code Ends
+/*
+ A binary tree node has data, pointer to left child
+   and a pointer to right child 
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+*/
+
+/* Function to print nodes of extreme corners
+of each level in alternate order */
+void printExtremeNodes(Node* root)
+{
+    bool isLeft=0;
+    Node *node;
+    queue<Node *>q;
+    q.push(root);
+    while(!q.empty()){
+        int size = q.size(),printed=0;
+        int MaxSize = q.size();
+        while(size--){
+            node = q.front();
+            if(isLeft==1 and printed==0){
+                cout<<node->data<<" ";
+                printed=1;
+            }
+            q.pop();
+            if(node->left)
+                q.push(node->left);
+            if(node->right)
+                q.push(node->right);
+        }
+        if(isLeft==0){
+            cout<<node->data<<" ";
+        }
+        isLeft = !isLeft;
+    }
+}
